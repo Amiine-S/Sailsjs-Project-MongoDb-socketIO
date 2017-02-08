3994b1955247c695cc26d788f4681a62d2637170
Amine <Amine.serra@gmail.com>
2017-02-08 15:56:22 +0100

    Update functions.js

--- AddFilesWebSocket/assets/js/functions.js
+++ AddFilesWebSocket/assets/js/functions.js
@@ -2,7 +2,7 @@
 
   	$('#poke').click(function () {
 		
-		io.socket.post('/home/poke', { name: 'Amine' }, function (resData, jwRes){
+		io.socket.post('/home/poke', function (resData, jwRes){
   			console.log(jwRes.statusCode);
   			console.log(resData);
   			$('#Infos').text("Message server : " + resData);
